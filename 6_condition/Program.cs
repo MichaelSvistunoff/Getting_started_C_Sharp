9f6ee078ce6f031c6cde5c044154090bc3cb63a3
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-21 15:10:26 +0600

    Программа с условием вывода по имени пользователя

---
+++ 6_condition/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter Your number: ");           // Input user name
+string userName = Console.ReadLine();               // Initial user name
+
+if(userName.ToLower()=="Michael") {                 // Condition user name = Michael
+    Console.WriteLine("Hello, Bro!");               // Print Bro
+} else {                                            // Condition user name != Michael
+    Console.WriteLine($"Hello, {userName}!");       // Brint default template
+}
