df4a6ef717cb1cd868c2a740afc129ebcadce1ba
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-21 14:41:58 +0600

    Программа умножения двух чисел, введенных пользователем

---
+++ 3_Example_summary/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter first number: ");                          // Input user number one
+int numOne = int.Parse(Console.ReadLine());                         // Initial user number one
+
+Console.WriteLine("Enter second number: ");                         // Input user number one
+int numTwo = int.Parse(Console.ReadLine());                         // Initial user number two
+
+int summary = numOne*numTwo;                                        // Summary of two numbers
+
+Console.WriteLine($"The summary of Your numbers is {summary}");     // Print summary
