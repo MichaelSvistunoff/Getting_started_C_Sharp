745663d6c67d242e4084fb250b1350f1f3f07d84
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-21 14:34:00 +0600

    Программа приветствия пользователя по имени

---
+++ 2_Example_hello_user/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter Your name: ");     // Input user name
+string userName = Console.ReadLine();       // Initial user name
+
+Console.WriteLine($"Hello, {userName}!");   // Print user name
+
