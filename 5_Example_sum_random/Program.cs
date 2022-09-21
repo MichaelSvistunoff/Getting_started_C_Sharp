1236f5ad9309d5ac0f834c2cae7f3d455db75e7c
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-21 15:02:17 +0600

    Программа умножения двух рандомных чисел

---
+++ 5_Example_sum_random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numOne = new Random().Next(1, 10);                  // Initial first random number
+int numTwo = new Random().Next(1, 10);                  // Initial second random number
+
+int summary = numOne*numTwo;                            // Math operation
+
+Console.WriteLine($"{numOne} * {numTwo} = {summary}");  // Print summary
