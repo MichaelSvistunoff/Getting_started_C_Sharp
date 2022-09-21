02ecd377c1b76d80b1c44db7a8f5b6e3c0d1d54e
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-21 15:31:49 +0600

    Нахождение максимума из 9 чисел вручную

---
+++ 10-1_Max_of_9_numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿int numOne = 15;                            // Initial 1
+int numTwo = 21;                            // Initial 2
+int numThree = 39;                          // Initial 3
+
+int numFour = 12;                           // Initial 4
+int numFive = 189;                          // Initial 5
+int numSix = 33;                            // Initial 6
+
+int numSeven = 13;                          // Initial 7
+int numEight = 23;                          // Initial 8
+int numNine = 33;                           // Initial 9
+
+int max = numOne;                           // Max is number one
+
+if ( numOne > max ) max = numOne;           // if thisNum > max, max = thisNum
+if ( numTwo > max ) max = numTwo;           // if thisNum > max, max = thisNum
+if ( numThree > max ) max = numThree;       // if thisNum > max, max = thisNum
+if ( numFour > max ) max = numFour;         // if thisNum > max, max = thisNum
+if ( numFive > max ) max = numFive;         // if thisNum > max, max = thisNum
+if ( numSix > max ) max = numSix;           // if thisNum > max, max = thisNum
+if ( numSeven > max ) max = numSeven;       // if thisNum > max, max = thisNum
+if ( numEight > max ) max = numEight;       // if thisNum > max, max = thisNum
+if ( numNine > max ) max = numNine;         // if thisNum > max, max = thisNum
+
+Console.WriteLine(max);                     // Print result
