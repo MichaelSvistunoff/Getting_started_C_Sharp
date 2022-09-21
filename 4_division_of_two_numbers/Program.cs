53c7780b8dfcabde231f50ba0976602b6795375b
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-21 14:54:57 +0600

    Программа деления двух чисел, введенных пользователем

---
+++ 4_division_of_two_numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input first number: ");      // Input number one
+int numOne = int.Parse(Console.ReadLine());     // Initial number two
+
+Console.WriteLine("Input second number: ");     // Input number two
+int numTwo = int.Parse(Console.ReadLine());     // Initial number two
+
+if(numOne>numTwo) {                             // Condition if first > second
+    int result = numOne/numTwo;
+    Console.WriteLine($"The result of division is {result}");
+} else {                                        // Condition if first < second
+    Console.WriteLine("You have inputed incorrect firs number: it needs to be more than second number!");
+}
