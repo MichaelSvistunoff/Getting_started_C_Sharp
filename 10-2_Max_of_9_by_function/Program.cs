fc889673e44824db7ae340d42f3e6cbafdc92552
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-21 15:35:42 +0600

    Нахождение максимума из 9 чисел функцией

---
+++ 10-2_Max_of_9_by_function/Program.cs
@@ -0,0 +1,31 @@
+﻿int Max(int arg1, int arg2, int arg3) {
+    int result = arg1;
+    if (arg2> result) result = arg2;
+    if (arg3> result) result = arg3;
+    return result;
+}
+
+int numOne = 15;
+int numTwo = 21;
+int numThree = 39;
+
+int numFour = 12;
+int numFive = 189;
+int numSix = 33;
+
+int numSeven = 13;
+int numEight = 23;
+int numNine = 33;
+// Classical method
+// int max1 = Max(numOne, numTwo, numThree);
+// int max2 = Max(numFour, numFive, numSix);
+// int max3 = Max(numSeven, numEight, numNine);
+// int maximun = Max(max1, max2, max3);
+// Analogy
+int maximun = Max(
+                    Max(numOne, numTwo, numThree),
+                    Max(numFour, numFive, numSix),
+                    Max(numSeven, numEight, numNine)
+                );
+
+Console.WriteLine($"Maximun number is {maximun}");
