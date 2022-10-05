63992ccacb959573db4f890bdfaa2ed120b4e2b1
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-06 00:48:05 +0600

    Числа Фибоначчи

---
+++ 22_Fibonacci_numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿double fibonacci (int n) {
+    if (n==1 || n==2) {
+        return 1;
+    } else {
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
+for (int i = 1; i < 50; i++) {
+    Console.WriteLine($"f({i}) = {fibonacci(i)}");    
+}
