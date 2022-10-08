f653b2216b7c3922a85027b52ca596c73154c4e2
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-09 02:32:59 +0600

    Вывод текста на экран

---
+++ 23_Draw_text/Program.cs
@@ -0,0 +1,13 @@
+﻿string caption = "Intensive C# Demo text";
+int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;
+int screenHeigthPosition = Console.WindowHeight / 2;
+
+DrawText(caption, screenWidthPosition, screenHeigthPosition);
+
+DrawText(
+    text: caption,
+    left: screenWidthPosition,
+    top: screenHeigthPosition
+);
+
+// TODO: закончить программу!
