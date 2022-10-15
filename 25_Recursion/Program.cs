d40ab92d30b994228d274916885763da46f72a1c
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-16 05:50:36 +0600

    Вывод чисел от A до B через цикл и рекурсией

---
+++ 25_Recursion/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Собрать строку с числами от A до B, где a<=b
+*/
+
+Console.Write("Enter method: ");
+int isChoosen = int.Parse(Console.ReadLine()!);
+
+// Обычный метод через цикл For
+if (isChoosen == 1) {
+    Console.Write("Enter A: ");
+    int A = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter B: ");
+    int B = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine(ShowAToB(A, B));
+
+    string ShowAToB(int a, int b) {
+        string result = string.Empty;
+
+        for (int i=a; i<=b; i++) {
+            result += $"{i} ";
+        }
+        return result;
+    }
+}
+
+else {
+    Console.Write("Enter A: ");
+    int A = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter B: ");
+    int B = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine(ShowAToB(A, B));
+
+    string ShowAToB(int a, int b) {
+        if (a <= b) {
+            return (a + " " + ShowAToB(a+1, b));
+        } else {
+            return string.Empty;
+        }
+    }
+}
