66dc0cecf5bae7cc849d63550ed94b1620a9c194
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-16 06:07:12 +0600

    Сумма от 1 до N

---
+++ 26_Rec_Sum_1_to_n/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ВЫвести сумму чисел от 1 до N
+*/
+Console.Write("Enter N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetSumStd(N));
+Console.WriteLine(GetSumRec(N));
+
+int GetSumStd(int number) {
+    int result = 0;
+
+    for (int i=0; i<=number; i++) {
+        result += i;
+    }
+    return result;
+}
+
+int GetSumRec (int number) {
+    if (number == 0) {
+        return 0;
+    }
+    return number + GetSumRec(number-1);
+}
